8b1c137006c07d9383365cd18e16085bc592d6dd
Mabel Aikins Acquah <acquahmabel60@gmail.com>
2024-08-12 20:14:37 +0000

    header fixed

---
+++ src/components/head.jsx
@@ -0,0 +1,26 @@
+import { GoBell } from "react-icons/go"
+
+const Head = () => {
+  return (
+    <div className="flex justify-between items-center p-4">
+      <div>
+        <h1 className="text-xs">Welcome Back!</h1>
+        <p className="text-xl font-semibold">Mabel</p>
+      </div>
+      <div className="flex items-center space-x-5">
+        <div className="hidden md:flex">
+          <input type="text" placeholder='search...' className="bg-indigo-100/30 px-4 py-2 rounded:lg focus:outline-0 focus:ring-2 focus:ring-indigo-600"/>
+        </div>
+        <div className="flex items-center space-x-5">
+          <button className="relative text-2xl text-gray-600">
+            <GoBell size={32}/>
+            <span className="absolute top-0 right-0 -mt-1 -mr-1 flex justify-center items-center bg-indigo-600 text-white font-semibold text-[10px] w-5 h-5 rounded-full border-2 border-white">9</span>
+            <img className="w-8 g-8 rounded-full border-4 border-indigo-400" src="" alt="" />
+          </button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Head
--- src/pages/dashboard2/index.jsx
+++ src/pages/dashboard2/index.jsx
@@ -2,7 +2,7 @@
 
 import { Outlet } from 'react-router-dom';
 import AdminSidebar from '../../components/sidebar';
-import Header from '../../components/header';
+import Head from '../../components/head';
 
 const AdminDashboard = () => {
   return (
@@ -10,7 +10,7 @@ const AdminDashboard = () => {
     <div className='flex'>
         <AdminSidebar />
         <div className='w-full ml-16 md:ml-56'>
-            <Header/>
+            <Head/>
             <Outlet />
         </div>
     </div>
